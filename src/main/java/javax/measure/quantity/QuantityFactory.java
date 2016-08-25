9bbf23515c05645220f08aa1c5936b126348efcc
Werner Keil <werner.keil@gmx.net>
2016-08-25 23:39:38 +0200

    Updated from trunk

--- src/main/java/javax/measure/quantity/QuantityFactory.java
+++ src/main/java/javax/measure/quantity/QuantityFactory.java
@@ -209,7 +209,6 @@ public abstract class QuantityFactory<Q extends Quantity<Q>>  {
         }
 
         @SuppressWarnings("unchecked")
-        @Override
         public Object invoke(final Object proxy, final Method method, final Object[] args) {
             final String name = method.getName();
             if (name.equals("doubleValue")) { // Most frequent.
